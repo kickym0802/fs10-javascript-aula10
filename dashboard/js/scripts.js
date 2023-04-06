86f559edc4b81606c668d8f81a398ae073fdd412
gleidsonteixeira <gleidsondeveloper@gmail.com>
2023-04-05 21:40:01 -0300

    projeto final javascript

--- dashboard/js/scripts.js
+++ dashboard/js/scripts.js
@@ -1,4 +1,10 @@
 let troca = document.querySelector(".troca");
+let body = document.querySelector("body");
+let header = document.querySelector("header");
+let main = document.querySelector("main");
 troca.addEventListener("click", function(){
     troca.classList.toggle("active");
+    body.classList.toggle("dark");
+    header.classList.toggle("active");
+    main.classList.toggle("active");
 });
